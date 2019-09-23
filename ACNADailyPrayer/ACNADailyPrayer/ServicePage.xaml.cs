200bf6a1649098e4b400a4c13caafd389e8633b6
Luke Childs <childsluke@hotmail.com>
2019-09-23 01:07:08 -0700

    Re-factored Service Xamarin Page to draw labels dynamically per service item - this fixed an issue in iOS regarding label max line capability.
    READY TO TEST!!!

--- ACNADailyPrayer/ACNADailyPrayer/ServicePage.xaml.cs
+++ ACNADailyPrayer/ACNADailyPrayer/ServicePage.xaml.cs
@@ -17,28 +17,41 @@ namespace ACNADailyPrayer
         public Page1(Service servicePushed)
         {
             InitializeComponent();
-
-            serviceTextLabel.FormattedText = new FormattedString();
+            //serviceTextLabel.FormattedText = new FormattedString();
+            stackLayout.Padding = new Thickness(this.Width * 0.05, 0, this.Width * 0.05, 0);
             int i = 0;
 
             foreach(string s in servicePushed.serviceText)
             {
+                Label entryLabel = new Label();
+
                 FormattedString serviceFormattedText = new FormattedString();
                 
                 // Extract the title and format different to body 
-                serviceFormattedText.Spans.Add(new Span { Text = s.Split('\n')[0] + "\n", FontAttributes = FontAttributes.Bold, TextColor = Xamarin.Forms.Color.Black, FontSize = Device.GetNamedSize(NamedSize.Title, typeof(Label)) }) ;
+                serviceFormattedText.Spans.Add(new Span { Text = s.Split('\n')[0] + "\n", FontAttributes = FontAttributes.Bold, TextColor = Color.Black, FontSize = Device.GetNamedSize(NamedSize.Title, typeof(Label)) }) ;
 
                 // Extact the body as regular text
-                serviceFormattedText.Spans.Add(new Span { Text = (s.Remove(0, serviceFormattedText.Spans[0].Text.Length - 1) + "\n"), TextColor = Xamarin.Forms.Color.DarkGray, FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) });
+                serviceFormattedText.Spans.Add(new Span { Text = (s.Remove(0, serviceFormattedText.Spans[0].Text.Length - 1) + "\n"), TextColor = Color.DarkGray, FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) });
+
+                //serviceTextLabel.FormattedText.Spans.Add(serviceFormattedText.Spans[0]);
+                //serviceTextLabel.FormattedText.Spans.Add(serviceFormattedText.Spans[1]);
+
+                entryLabel.FormattedText = new FormattedString();
+                entryLabel.FormattedText.Spans.Add(serviceFormattedText.Spans[0]);
+                entryLabel.FormattedText.Spans.Add(serviceFormattedText.Spans[1]);
+
                 
-                serviceTextLabel.FormattedText.Spans.Add(serviceFormattedText.Spans[0]);
-                serviceTextLabel.FormattedText.Spans.Add(serviceFormattedText.Spans[1]);
-                
-                //serviceTextLabel.FormattedText ++ s;
-                //serviceTextLabel.FormattedText += "\n";
+                stackLayout.Children.Add(entryLabel);
 
                 i++;
             }
+   
         }
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height); //must be called
+            stackLayout.Padding = new Thickness(this.Width * 0.05, 0, this.Width * 0.05, 0);
+        }
+
     }
 }
