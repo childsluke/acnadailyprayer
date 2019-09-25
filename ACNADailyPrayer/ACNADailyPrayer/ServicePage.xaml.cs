53a29488cb08e0a0da12443f6987fe67087e10b3
Luke Childs <childsluke@hotmail.com>
2019-09-25 09:15:51 -0700

    v2 alpha continued: - Platform-specific font loading in-place. Tested working on Android, need to test on iOS.
    //----------------------------------------//
    
    TODO:
    - Alpha test with cohort
    - Figure out Apocrypha
    - Seek out appropriate permissions on BCP 2019 / New Coverdale Psalter usage
    
    NICE-TO-HAVEs:
    - Intercession list sub-app
    - More services
    - Psalter translation selection
    - Bible trandslation selection

--- ACNADailyPrayer/ACNADailyPrayer/ServicePage.xaml.cs
+++ ACNADailyPrayer/ACNADailyPrayer/ServicePage.xaml.cs
@@ -26,6 +26,24 @@ namespace ACNADailyPrayer
             //serviceTextLabel.FormattedText = new FormattedString();
             stackLayout.Padding = new Thickness(this.Width * 0.1, this.Height * 0.05, this.Width * 0.075, this.Height * 0.05);
             int i = 0;
+
+
+            // Platform-specific font loading
+            string regularFont = "";
+            string boldFont = "";
+
+            switch(Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    regularFont = "Cormorant Garamond - Regular";
+                    boldFont = "Cormorant Garamond";
+                    break;
+
+                case Device.Android:
+                    regularFont = "CormorantGaramond-Regular.ttf#Cormorant Garamond - Regular";
+                    boldFont = "CormorantGaramond-Bold.ttf#Cormorant Garamond";
+                    break;
+            }
             
 
             foreach(string s in servicePushed.serviceText)
@@ -35,10 +53,10 @@ namespace ACNADailyPrayer
                 FormattedString serviceFormattedText = new FormattedString();
                 
                 // Extract the title and format different to body 
-                serviceFormattedText.Spans.Add(new Span { Text = s.Split('\n')[0] + "\n",/*FontAttributes = FontAttributes.Bold,*/ TextColor = Color.Black, FontFamily = "CormorantGaramond-Bold.ttf#Cormorant Garamond", FontSize = Device.GetNamedSize(NamedSize.Title, typeof(Label)) }) ;
+                serviceFormattedText.Spans.Add(new Span { Text = s.Split('\n')[0] + "\n",/*FontAttributes = FontAttributes.Bold,*/ TextColor = Color.Black, FontFamily = boldFont, FontSize = Device.GetNamedSize(NamedSize.Title, typeof(Label)) }) ;
 
                 // Extact the body as regular text
-                serviceFormattedText.Spans.Add(new Span { Text = (s.Remove(0, serviceFormattedText.Spans[0].Text.Length - 1) + "\n"), TextColor = Color.Black, FontFamily="CormorantGaramond.ttf#Cormorant Garamond", FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) });
+                serviceFormattedText.Spans.Add(new Span { Text = (s.Remove(0, serviceFormattedText.Spans[0].Text.Length - 1) + "\n"), TextColor = Color.Black, FontFamily=regularFont, FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) });
 
                 //serviceTextLabel.FormattedText.Spans.Add(serviceFormattedText.Spans[0]);
                 //serviceTextLabel.FormattedText.Spans.Add(serviceFormattedText.Spans[1]);
